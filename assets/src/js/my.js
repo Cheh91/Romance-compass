acd4de07059294cc84f976ac82e7a7071dc12587
cheh91 <4eh91@ukr.net>
2020-12-04 17:58:27 +0200

    added start profile girl

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -4,20 +4,38 @@
 
 jQuery(document).ready(function () {
     initDropdown();
+    openHeaderMenu();
 
 });
 
     function initDropdown(){
-        $(".search__drop-menu").click(function () {
+        $(".search__drop-menu").click(function (e) {
+          e.preventDefault();
             $(this).parent().toggleClass("active");
             // console.log("привет")
         })
 
-        $(".search__drop-sub-menu li").click(function(){
+        $(".search__drop-sub-menu li").click(function(e){
+          e.preventDefault();
             let currentele = $(this).html();
             $(".search__drop-menu li").html(currentele);
             $(this).parents(".search__drop-item").removeClass("active");
         })
+    }
+
+    function openHeaderMenu(){
+      // $(".header__btn-open").click(function(){
+      //   $(".header__nav").show();
+      // })
+
+      // $(".header__nav-btn-close").click(function(){
+      //   $(".header__nav").hide();
+      // })
+
+      $(".header__btn-open").click(function(){
+        $(".header__btn-open span").toggleClass("active")
+        $(".header__nav").toggle(500);
+      })
     }
 
 

1d0f20a960f18fbacd8844cf935bcd6443adea8f
cheh91 <4eh91@ukr.net>
2020-12-08 18:00:04 +0200

    added gallery block

--- assets/build/js/my.js
+++ assets/build/js/my.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// (function () {
 
 jQuery(document).ready(function () {
     initDropdown();
@@ -10,34 +9,25 @@ jQuery(document).ready(function () {
 });
 
     function initDropdown(){
-        $(".search__drop-menu").click(function (e) {
-          e.preventDefault();
-            $(this).parent().toggleClass("active");
-            // console.log("привет")
-        })
-
-        $(".search__drop-sub-menu li").click(function(e){
-          e.preventDefault();
-            let currentele = $(this).html();
-            $(".search__drop-menu li").html(currentele);
-            $(this).parents(".search__drop-item").removeClass("active");
-        })
-    }
-
-    function openHeaderMenu(){
-      // $(".header__btn-open").click(function(){
-      //   $(".header__nav").show();
-      // })
-
-      // $(".header__nav-btn-close").click(function(){
-      //   $(".header__nav").hide();
-      // })
-
-      $(".header__btn-open").click(function(){
-        $(".header__btn-open span").toggleClass("active")
-        $(".header__nav").toggle(500);
+      $(".drop-box").click(function (e) {
+        e.preventDefault();
+        $(this).find(".drop-menu").toggleClass("active");
       })
     }
+
+    // function openHeaderMenu(){
+    //   $(".header__btn-open").click(function(){
+    //     $(".header__btn-open span").toggleClass("active")
+    //     $(".header__nav").toggle(500);
+    //   })
+    // }
+
+function openHeaderMenu() {
+  $(".header__btn-open").click(function () {
+    $(".header__btn-open span").toggleClass("active")
+    $(".header__nav").toggleClass("open");
+  })
+}
 
     function initSlider(){
       $('.profile__slider').slick({
@@ -47,7 +37,6 @@ jQuery(document).ready(function () {
         slidesToShow: 4,
         slidesToScroll: 1,
         variableWidth: true,
-        // Padding: "120px",
       })
       
   }
